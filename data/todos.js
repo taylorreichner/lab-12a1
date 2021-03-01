89188bb9616e369368f67886ad509e4e970e0091
taylorreichner <taylorreichner@yahoo.com>
2021-03-01 15:57:56 -0800

    got my own data set injected

---
+++ data/todos.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        id: 1,
+        todo: 'wash the dishes',
+        completed: false,
+        user_id: 4
+    },
+
+{
+    id: 2,
+    todo: 'clean the floor',
+    completed: false,
+    user_id: 4
+},
+{
+    id: 3,
+    todo: 'rake the leaves',
+    completed: false,
+    user_id: 4
+},
+
+]
